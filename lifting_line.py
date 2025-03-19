7ce497c3a81b31becb42255403f9136c174b61ad
XerusN <134720540+XerusN@users.noreply.github.com>
2025-03-19 10:52:30 +0100

    not converging

--- lifting_line.py
+++ lifting_line.py
@@ -4,6 +4,7 @@ from InductionFactors import *
 from SingularIntegration import *
 from numpy import pi
 from utils import *
+from scipy import interpolate
 
 # Unless otherwise specified all units are SI
 
@@ -91,7 +92,9 @@ def import_geometry(path: list) -> Geometry:
     
 # Interpolate Lift coefficient in the xfoil data
 def cl_from_xfoil(a: float, cl_a, section: int):
-    cl = np.interp(rad_to_deg(a), cl_a[section, :, 0], cl_a[section, :, 1])
+    cl_cubic = interpolate.CubicSpline(cl_a[section, :, 0], cl_a[section, :, 1])
+    #cl = np.interp(rad_to_deg(a), cl_a[section, :, 0], cl_a[section, :, 1])
+    cl = cl_cubic(rad_to_deg(a))
     return cl
 
 # n and V from prescribed Reynolds and j
@@ -234,7 +237,7 @@ def q3():
             for section in range(len(prop.geo.r_R)-1):
                 if ut[section] == 0.:
                     ua[section] = 0.1
-                beta = np.arctan(ut[section]/ua[section])
+                beta = np.arctan((v[i] + ua[section]/2.)/(prop.geo.r_R[section]*prop.char.d*pi*n[i] - ut[section]/2.))
                 aoa_ = aoa(beta, prop, section)
                 cl[section] = cl_from_xfoil(aoa_, cl_a, section)
             v_inf = np.sqrt((v[i] + ua/2.)**2 + (prop.geo.r_R[:-1]*prop.char.d*pi*n[i] - ut/2.)**2)
@@ -259,7 +262,7 @@ def q3():
         ut = gamma/(pi*prop.geo.r_R[:-1]*prop.char.d)
         delta = v[i]**2 + ut*(2.*pi*n[i]*prop.geo.r_R[:-1]*prop.char.d-ut)
         ua = -v[i]+np.sqrt(delta)
-        beta = np.arctan(ut/ua)
+        beta = np.arctan((v[i] + ua/2.)/(prop.geo.r_R[:-1]*prop.char.d*pi*n[i] - ut/2.))
         for section in range(len(prop.geo.r_R)-1):
             if ut[section] == 0.:
                 ua[section] = 0.1
@@ -289,16 +292,22 @@ def q3():
     
     export('q3.txt', j, kt, kq, eta)
 
+def sinspace(start, end, num=50):
+    # Create a sinusoidal distribution over the interval
+    t = np.linspace(0, np.pi, num)  # Create a sine-shaped spacing
+    s = (1 - np.cos(t)) / 2         # Map cosine to [0, 1] for smoother spacing
+    return start + (end - start) * s
+
 # Lifting line with induction factors
 def q4():
     
     # iterations control varialbes
-    max_it = 200
+    max_it = 3000
     tol = 1e-6
-    relax = 0.1
+    relax = 0.01
     
     # Span-wise discretisation (can be higher than the number of provided foil sections)
-    n_span = 20
+    n_span = 40
     
     rn07, water, prop, j, cl_a = setup()
     
@@ -316,7 +325,9 @@ def q4():
         
         # _2 indicates that the discretisation is the one controlled by n_span and not the provided geometry
         gamma_2 = np.ones(n_span)*0.1
+        #gamma_2 = np.array([-((float(l) - float(n_span)/2.)/float(n_span))**2 + 1. for l in range(n_span)])
         gamma_2[0] = 0.
+        gamma_2[-1] = 0.
         gamma_new_2 = np.zeros(n_span)
         cl = np.zeros(len(prop.geo.r_R))    # To ensure proper gamma at the tip, see later
         cl_2 = np.zeros(n_span)
@@ -326,64 +337,86 @@ def q4():
         beta = np.zeros(len(prop.geo.r_R)-1)
         beta_2 = np.zeros(n_span)
         r_2 = np.linspace(prop.geo.r_R[0]*prop.char.d/2., prop.geo.r_R[-1]*prop.char.d/2., n_span)
+        #r_2 = sinspace(prop.geo.r_R[0]*prop.char.d/2., prop.geo.r_R[-1]*prop.char.d/2., n_span)
         beta_2[:] = np.arctan(v[i]/(2*pi*r_2*n[i]))
+        c_2 = np.interp(r_2, prop.geo.r_R*prop.char.d/2., prop.geo.c_d*prop.char.d)
+        
+        plt.plot(r_2, gamma_2, 'x-', label=k)
         
         while k < max_it and rel_diff > tol:
             # Lift from hydrodynamic pitch angle
             for section in range(len(prop.geo.r_R)-1):
                 aoa_ = aoa(beta[section], prop, section)
                 cl[section] = cl_from_xfoil(aoa_, cl_a, section)
-            cl[-1] = 0.     # Ensures proper gamma at the tip
-            print('cl ', cl)
+            #cl[0] = 0.         #debug
+            cl[-1] = 0.     # No foil at the tip
+            #print('cl ', cl)
             cl_2 = np.interp(r_2, prop.geo.r_R[:]*prop.char.d/2., cl)
             v_inf_2 = np.sqrt((v[i] + ua_2/2.)**2 + (2*r_2*pi*n[i] - ut_2/2.)**2)
             
+            #plt.plot(r_2, cl_2, 'x-', label=k+1)
+            
             # Gamma update, under relaxation to improve convergence
-            c_2 = np.interp(r_2, prop.geo.r_R*prop.char.d/2., prop.geo.c_d*prop.char.d)
-            gamma_new_2 = cl_2*v_inf_2*c_2/(2./prop.char.z)
+            gamma_new_2 = prop.char.z/2. * cl_2*v_inf_2*c_2
             if k == 0:
                 rel_diff = 1.
             else:
+                old_rel_diff = rel_diff
                 rel_diff = np.linalg.norm(gamma_new_2 - gamma_2) / np.mean(gamma_2)
+                # if abs(old_rel_diff-rel_diff)/rel_diff < 1e-4:
+                #     relax *= 2.if abs(old_rel_diff-rel_diff)/rel_diff < 1e-4:
+                #     relax *= 2.
             gamma_2 = gamma_2*(1-relax) + gamma_new_2*relax
             
+            gamma_2[0] = 0.
+            gamma_2[-1] = 0.
+            
+            # if rel_diff > 15.:
+            #     for l in range(1, n_span-1):
+            #         gamma_2[l] = 0.5*(gamma_2[l-1] + gamma_2[l+1])
+            
+            #plt.plot(r_2, gamma_2, 'x-', label=k+1)
             # Derivative of Gamma according to r
             dg_dr_2 = np.zeros(n_span)
-            dg_dr_2[0] = (gamma_2[1] - gamma_2[0])/(r_2[1] - r_2[0])
-            dg_dr_2[-1] = (gamma_2[-1] - gamma_2[-2])/(r_2[-1] - r_2[-2])
+            dg_dr_2[0] = (gamma_2[1] - gamma_2[0])/(r_2[1] - r_2[0])#/prop.char.z
+            dg_dr_2[-1] = (gamma_2[-1] - gamma_2[-2])/(r_2[-1] - r_2[-2])#/prop.char.z
             for l in range(1, n_span-1):
-                dg_dr_2[l] = (gamma_2[l+1] - gamma_2[l-1])/(r_2[l+1] - r_2[l-1])
+                dg_dr_2[l] = (gamma_2[l+1] - gamma_2[l-1])/(r_2[l+1] - r_2[l-1])#/prop.char.z
+            
+            plt.plot(r_2, dg_dr_2, 'x-', label=k+1)
             
             ia_2 = np.zeros(n_span)
             it_2 = np.zeros(n_span)
             for l in range(1,n_span-1):
                 # Induction factors
                 for m in range(n_span):
-                    ia_2[m], it_2[m] = inductionFactors(r_2[m], r_2[l], beta_2[l], prop.char.z)
-                ua_2[l] = 0.5*singularIntegration(r_2, dg_dr_2*ia_2, r_2[l])*v[i]
-                ut_2[l] = 0.5*singularIntegration(r_2, dg_dr_2*it_2, r_2[l])*v[i]
-            print('NaN in ut ua', np.sum(np.isnan(ut_2)), np.sum(np.isnan(ua_2)))
+                    ia_2[m], it_2[m] = inductionFactors(r_2[m], r_2[l], beta_2[m], prop.char.z)
+                ua_2[l] = singularIntegration(r_2, dg_dr_2*ia_2, r_2[l])/(2.*pi)
+                ut_2[l] = singularIntegration(r_2, dg_dr_2*it_2, r_2[l])/(2.*pi)
+            #print('NaN in ut ua', np.sum(np.isnan(ut_2)), np.sum(np.isnan(ua_2)))
             if np.sum(np.isnan(ut_2)) > 0:
+                plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+                plt.show()
+                print('EXIT - NaN Values')
                 exit()
-            ua_2[0] = 0.1
-            ua_2[-1] = 0.1
+            ua_2[0] = 0.
+            ua_2[-1] = 0.
             ut_2[0] = 0.
             ut_2[-1] = 0.
             
             # Hydrodynamic pitch angle
-            print('ut ua ', ut_2, ua_2)
-            beta_2 = np.arctan(ut_2/ua_2)
-            beta_2[0] = np.arctan(v[i]/(2*pi*r_2[0]*n[i]))
-            beta_2[-1] = np.arctan(v[i]/(2*pi*r_2[-1]*n[i]))
+            #print('ut ua ', ut_2, ua_2)
+            beta_2 = np.arctan((v[i] + ua_2/2.)/(2*r_2*pi*n[i] - ut_2/2.))
             beta = np.interp(prop.geo.r_R[:-1]*prop.char.d/2., r_2, beta_2)
             k += 1
-            print('beta ', beta_2)
+            #print('beta ', beta_2)
             if k % 1 == 0:
-                print(k, rel_diff)
-                print('ooooooooooo')
+                print(k, rel_diff, np.sum(gamma_2), relax)
+            #print('ooooooooooo')
         
         print(k, rel_diff)
         
+        plt.plot(r_2, gamma_2, label=i)
         # Derivative of Gamma according to r
         dg_dr_2 = np.zeros(n_span)
         dg_dr_2[0] = (gamma_2[1] - gamma_2[0])/(r_2[1] - r_2[0])
@@ -406,7 +439,7 @@ def q4():
         ut_2[-1] = 0.
         
         # Hydrodynamic pitch angle
-        beta_2 = np.arctan(ut_2/ua_2)
+        beta_2 = np.arctan((v[i] + ua_2/2.)/(2*r_2*pi*n[i] - ut_2/2.))
         beta = np.interp(prop.geo.r_R[:-1]*prop.char.d/2., r_2, beta_2)
         ut = np.interp(prop.geo.r_R[:-1]*prop.char.d/2., r_2, ut_2)
         ua = np.interp(prop.geo.r_R[:-1]*prop.char.d/2., r_2, ut_2)
@@ -438,8 +471,12 @@ def q4():
         T[i] += dT_2.sum()
         Q[i] += dQ_2.sum()
         
+        print(T*prop.char.z /(water.density * n**2 * prop.char.d**4))
+        exit()
+        
         print("----------")
     
+    plt.show()
     # K_T, K_Q and efficiency
     kt = T*prop.char.z /(water.density * n**2 * prop.char.d**4)
     kq = Q*prop.char.z /(water.density * n**2 * prop.char.d**5)
@@ -447,17 +484,17 @@ def q4():
     
     export('q4.txt', j, kt, kq, eta)
     
-print("***************************")
-print("q2")
-print("***************************")
-q2()
-print()
-print("***************************")
-print("q3")
-print("***************************")
-print()
-q3()
-print()
+# print("***************************")
+# print("q2")
+# print("***************************")
+# q2()
+# print()
+# print("***************************")
+# print("q3")
+# print("***************************")
+# print()
+# q3()
+# print()
 print("***************************")
 print("q4")
 print("***************************")
